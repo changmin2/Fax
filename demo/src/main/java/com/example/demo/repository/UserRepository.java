d6fb1a9c452bc1f729f9642345fb8459395261ce
LeeChangMin <77597971+changmin2@users.noreply.github.com>
2023-01-26 13:41:48 +0900

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.User;
-import com.example.demo.domain.UserPK;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, UserPK> {
-}
