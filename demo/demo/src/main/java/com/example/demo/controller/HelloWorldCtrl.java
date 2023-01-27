10745929176a86fc2ae569a09b36d579d89cdf76
leechangmin20173078 <77597971+leechangmin20173078@users.noreply.github.com>
2023-01-27 14:43:44 +0900

    d

--- demo/demo/src/main/java/com/example/demo/controller/HelloWorldCtrl.java
+++ demo/demo/src/main/java/com/example/demo/controller/HelloWorldCtrl.java
@@ -1,15 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.ReceiveService;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class HelloWorldCtrl {
+
+    private final ReceiveService receiveService;
 
     @GetMapping("/hello")
     public String helloWorld() {
         return "hello!";
+    }
+
+    @GetMapping("/onlytest")
+    public void test() throws IOException, ParseException {
+        receiveService.Receive();
     }
 }
--- demo/demo/src/main/java/com/example/demo/service/ReceiveService.java
+++ demo/demo/src/main/java/com/example/demo/service/ReceiveService.java
@@ -4,26 +4,44 @@ import classes.Multipart.HttpPostMultipart;
 import com.example.demo.GlobalVariables;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ReceiveService {
-//
-//    private final GlobalVariables globalVariables;
-////
-////    public String Receive(){
-////        HttpPostMultipart multipart = new HttpPostMultipart("https://balsong.com/Linkage/API/", "utf-8", headers);
-//
-////        //데이터 (요청변수 대소문자 구분)
-////        multipart.addFormField("UserID", globalVariables.getFaxId());
-////        multipart.addFormField("UserPW", globalVariables.getFaxPw());
-////        multipart.addFormField("Service", globalVariables.getService());
-////        multipart.addFormField("Type", "Send");
-////        multipart.addFormField("Send_Date", req.getSend_Date());
-//        return null;
-//    }
+
+    private final GlobalVariables globalVariables;
+
+    public String Receive() throws IOException, ParseException {
+        Map<String, String> headers = new HashMap<>();
+        HttpPostMultipart multipart = new HttpPostMultipart("https://balsong.com/Linkage/API/", "utf-8", headers);
+
+        //데이터 (요청변수 대소문자 구분)
+        multipart.addFormField("UserID", globalVariables.getFaxId());
+        multipart.addFormField("UserPW", globalVariables.getFaxPw());
+        multipart.addFormField("Service", "RFAX");
+        multipart.addFormField("Type", "Report");
+        multipart.addFormField("RFax_No", "050-4926-0237");
+        multipart.addFormField("Date_Start", "2023-01-01");
+        multipart.addFormField("Date_End", "2023-01-27");
+        multipart.addFormField("List_EA", "10");
+        multipart.addFormField("Page", "1");
+
+        String ResultJson = multipart.finish();
+        // Json parse (json.simple 라이브러리)
+        JSONParser jsonParse = new JSONParser();
+        JSONObject ObjToJson = (JSONObject) jsonParse.parse(ResultJson);
+        log.info(ObjToJson.toString());
+        return null;
+    }
 }
