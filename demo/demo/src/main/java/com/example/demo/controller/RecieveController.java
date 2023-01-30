98d3b90d5c3d9792995af0c5c8eb0abb3a4d02ee
pjb <125371126@naver.com>
2023-01-30 17:56:37 +0900

    crossorigin 추가

--- demo/demo/src/main/java/com/example/demo/controller/RecieveController.java
+++ demo/demo/src/main/java/com/example/demo/controller/RecieveController.java
@@ -9,14 +9,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(
+        // localhost:5500 과 127.0.0.1 구분
+        origins = "http://localhost:5500", // allowCredentials = "true" 일 경우, orogins="*" 는 X
+        allowCredentials = "true",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT,RequestMethod.HEAD,RequestMethod.OPTIONS}
+)
 @RestController
 @Slf4j
 @RequestMapping("/api")
