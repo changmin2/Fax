a58323a41e39cca0da96764fcb0faedda3f4bf81
suky <1724271@donga.ac.kr>
2023-02-03 09:18:33 +0900

    메인페이지 수정

--- demo/demo/frontend/src/views/Landing.vue
+++ demo/demo/frontend/src/views/Landing.vue
@@ -2,75 +2,81 @@
   <section class="section section-shaped section-hero login-section section-lg my-0">
     <div class="container-fluid mt-5">
       <div class="row">
-        <div class="col left-content"></div>
+        <div class="col left-content">
+          <div class="content-group">
+            <div class="send-title font-weight-700 text-default">총건수</div>
+            <hr />
+            <div class="landing-total">
+              <div class="row landing-total-group" style="width: 5rem">
+                <span style="text-align: center">미확인</span>
+                <span class="landing-total-num" type="secondary">{{ data.ReceiveCount }}건</span>
+              </div>
+              <div class="row landing-total-group" style="width: 5rem">
+                <span style="text-align: center">미결재</span>
+                <span class="landing-total-num" type="secondary">{{ data.NotApprCount }}건</span>
+              </div>
+            </div>
+          </div>
+          <div class="content-group">
+            <div class="send-title font-weight-700 text-default">
+              <i class="fa fa-volume-down" aria-hidden="true"></i> 공지사항
+            </div>
+            <div class="table-container">
+              <hr />
+              <table class="fax-table content-group-notice">
+                <!-- <tr class="ApprArea-header">
+                  <th>내용</th>
+                  <th>제목</th>
+                </tr> -->
+                <tr v-for="(notice, index) in data.NoticeInfo" :key="index">
+                  <td>{{ notice.CONTENT }}</td>
+
+                  <td style="width: 8rem">{{ notice.DATE }}</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
         <div class="col right-content">
-          <div class="send-title font-weight-700 text-default">받은팩스</div>
-          <div class="table-container">
-            <hr />
-            <table class="fax-table">
-              <tr class="ApprArea-header">
-                <th>구분</th>
-                <th>제목</th>
-                <th>보낸사람</th>
-                <th>읽은사람</th>
-                <th>받은날짜</th>
-              </tr>
-
-              <tr v-for="(receive, index) in receivelist" :key="index">
-                <td>{{ receive.read_YN }}</td>
-                <td>{{ receive.title }}</td>
-                <td>{{ receive.sender_NO }}</td>
-                <td>{{ receive.read_USER }}</td>
-                <td>{{ receive.receive_No_SEQ }}</td>
-              </tr>
-            </table>
-            <hr />
+          <div class="content-group">
+            <div class="send-title font-weight-700 text-default">미확인</div>
+            <div class="table-container">
+              <hr />
+              <table class="fax-table">
+                <tr class="ApprArea-header">
+                  <th>발신자팩스번호</th>
+                  <th>페이지수</th>
+                  <th>수신일시</th>
+                </tr>
+                <tr v-for="(receive, index) in data.ReceiveLsit" :key="index">
+                  <td>{{ receive.SENDER_NO }}</td>
+                  <td>{{ receive.PAGE_CNT }}</td>
+                  <td>{{ receive.RECEIVE_DATE }}</td>
+                </tr>
+              </table>
+            </div>
           </div>
-          <div class="send-title font-weight-700 text-default">보낸팩스</div>
-          <div class="table-container">
-            <hr />
-            <table class="fax-table">
-              <tr class="ApprArea-header">
-                <th>구분</th>
-                <th>제목</th>
-                <th>결재상태</th>
-                <th>요청자</th>
-                <th>요청일</th>
-              </tr>
+          <div class="content-group">
+            <div class="send-title font-weight-700 text-default">
+              <i class="fa fa-exclamation-circle" aria-hidden="true"></i> 미결재
+            </div>
 
-              <tr v-for="(send, index) in sendList" :key="index">
-                <td>{{ send.결재상태 }}</td>
-                <td>{{ send.제목 }}</td>
-                <td>{{ send.결재상태 }}</td>
-                <td>{{ send.발송자 }}</td>
-                <td>{{ send.전송일자 }}</td>
-              </tr>
-            </table>
-            <hr />
-          </div>
+            <div class="table-container">
+              <hr />
+              <table class="fax-table">
+                <tr class="ApprArea-header">
+                  <th>제목</th>
+                  <th>기안자</th>
+                  <th>등록일</th>
+                </tr>
 
-          <div class="send-title font-weight-700 text-default">결재문서</div>
-
-          <div class="table-container">
-            <hr />
-            <table class="fax-table">
-              <tr class="ApprArea-header">
-                <th>구분</th>
-                <th>제목</th>
-                <th>결재상태</th>
-                <th>요청자</th>
-                <th>요청일</th>
-              </tr>
-
-              <tr v-for="(noApproval, index) in noApprovalList" :key="index">
-                <td>{{ noApproval.상태 }}</td>
-                <td>{{ noApproval.제목 }}</td>
-                <td>{{ noApproval.상태 }}</td>
-                <td>{{ noApproval.보내는사람 }}</td>
-                <td>{{ noApproval.요청일자 }}</td>
-              </tr>
-            </table>
-            <hr />
+                <tr v-for="(noApproval, index) in data.NotApprList" :key="index">
+                  <td>{{ noApproval.TITLE }}</td>
+                  <td>{{ noApproval.NAME }}</td>
+                  <td>{{ noApproval.INSERT_DATE }}</td>
+                </tr>
+              </table>
+            </div>
           </div>
         </div>
       </div>
@@ -91,6 +97,10 @@ export default {
       receivelist: [],
       sendList: [],
       noApprovalList: [],
+
+      searchFrom: "",
+      searchTo: "",
+      data: [],
     };
   },
   computed: {
@@ -99,53 +109,37 @@ export default {
     }),
   },
   created() {
-    this.getReceiveList();
-
-    this.getSendList();
-    this.noApproval();
+    this.getMainInfo();
   },
   methods: {
-    // 받은팩스
-    async getReceiveList() {
-      this.$store.commit("SET_LOADING_TRUE");
-      try {
-        let response = await http.post("/recieveList", {
-          RFax_No: this.userInfo.faxNo,
-          Date_Start: "2023-01-29",
-          Date_End: "2023-02-01",
-        });
+    // landing 페이지 날짜 -> 현재시간 ~ 일주일 전까지
+    getNow() {
+      const today = new Date();
+      const year = today.getFullYear(); // 년
+      const month = today.getMonth(); // 월
+      const day = today.getDate(); // 일
 
-        let { data } = response;
-        this.$store.commit("SET_LOADING_FALSE");
-
-        if (data != null) {
-          // 전송 성공
-          if (data.length > 3) data = data.splice(0, 3);
-          console.log(data);
-          console.log("전송 성공");
-          this.receivelist = data;
-          this.detailOpen = false;
-        } else {
-          console.log("전송 실패");
-        }
-      } catch (error) {
-        // 전송 실패
-        console.log("오류메시지 - ", data.Message);
-        alertify.error("실패했습니다.", 1.5);
-      }
+      this.searchFrom = new Intl.DateTimeFormat("fr-CA", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      }).format(new Date(year, month, day - 6));
+      this.searchTo = new Intl.DateTimeFormat("fr-CA", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      }).format(Date.now());
+      this.setDateInfo(this.searchFrom);
+      this.setDateInfo(this.searchTo);
+      console.log(this.searchFrom + " ~ " + this.searchTo);
     },
 
-    // 보낸팩스함 조회
-    async getSendList() {
+    // mainInfo
+    async getMainInfo() {
       this.$store.commit("SET_LOADING_TRUE");
       try {
-        let response = await http.post("/sendRecieve", {
+        let response = await http.post("/mainInfo", {
           userId: this.userInfo.userId,
-          // searchFrom: this.searchFrom,
-          // searchTo: this.searchTo,
-          searchFrom: "2023-01-29",
-          searchTo: "2023-02-01",
-          status: "전체",
         });
 
         let { data } = response;
@@ -153,11 +147,11 @@ export default {
 
         if (data != null) {
           // 전송 성공
-          if (data.length > 3) data = data.splice(0, 3);
+          // if (data.length > 3) data = data.splice(0, 3);
           console.log(data);
-
-          this.sendList = data;
-          this.detailOpen = false;
+          console.log("공지사항정보", data.NoticeInfo);
+          console.log("전송 성공");
+          this.data = data;
         } else {
           console.log("전송 실패");
         }
@@ -165,47 +159,33 @@ export default {
         // 전송 실패
         console.log("오류메시지 - ", data.Message);
         alertify.error("실패했습니다.", 1.5);
-      }
-    },
-    // 결재함 리스트
-    async noApproval() {
-      this.$store.commit("SET_LOADING_TRUE");
-
-      let formData = new FormData();
-      formData.append("userId", this.userInfo.userId);
-      formData.append("status", "전체"); // 상태 변수 추가 [대기, 전체, 완료, 회수, 반려]
-      formData.append("searchFrom", "2023-01-29"); // 조회기간 (시작)
-      formData.append("searchTo", "2023-02-01"); // 조회기간 (종료)
-      try {
-        let response = await http.post(`/payRecieve`, formData);
-        console.log(response);
-        let { data } = response;
-
-        // 랜딩 페이지에서 보이는 개수 조정
-        if (data.length > 3) data = data.splice(0, 3);
-
-        this.$store.commit("SET_LOADING_FALSE");
-
-        if (data != null) {
-          // 전송 성공
-          console.log(data);
-          console.log("결재함 조회 성공");
-          this.noApprovalList = data;
-
-          alertify.success("결재함 조회 완료되었습니다.", 1.5);
-        } else {
-          console.log("결재함 조회  실패");
-        }
-      } catch (error) {
-        // 전송 실패
-        console.log("오류메시지 - ", error);
-        alertify.error("결재함 조회에 실패했습니다.", 1.5);
       }
     },
   },
 };
 </script>
 <style scoped>
+.landing-total {
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+  margin-top: 2rem;
+  /* width: 100%; */
+}
+.landing-total-group {
+  width: 30rem;
+  margin: 1rem;
+}
+.landing-total-num {
+  background-color: rgb(239, 239, 239);
+  border: 1px solid rgb(200, 200, 200);
+  border-radius: 1rem;
+  width: 15rem;
+  height: 3rem;
+  text-align: center;
+  font-size: x-large;
+  line-height: 3rem;
+}
 .left-content {
   /* background-color: pink; */
   margin-left: 10px;
@@ -214,32 +194,40 @@ export default {
   /* background-color: aquamarine; */
   margin-right: 10px;
 }
+.content-group {
+  width: 30rem;
+  height: 20rem;
+}
 .send-title {
   font-size: large;
 }
 .table-container {
   width: 300px;
+  margin-bottom: 10px;
 }
-.search-area th,
-.search-area td {
-}
-
-th {
-  background-color: rgb(224, 224, 224);
-  font-weight: normal;
-  text-align: center;
-}
-
 .body-content {
   margin-top: 10px;
 }
 .fax-table {
   table-layout: fixed;
   width: 32rem;
-  height: 100px;
+  /* height: 100px; */
 }
 .fax-table td {
   height: 2.3rem;
+}
+.fax-table-landing {
+  width: 12rem;
+  text-align: center;
+}
+
+.fax-table-landing {
+}
+.fax-table-landing th {
+  background-color: white;
+}
+.fax-table-landing td {
+  background-color: gainsboro;
 }
 /* .ApprArea-header th {
   background-color: rgb(224, 224, 224);
@@ -265,7 +253,15 @@ th {
 
 hr {
   width: 32rem;
-  margin-top: 15px;
-  margin-bottom: 15px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.content-group-notice {
+  border: 1px solid gainsboro;
+}
+
+.admin-container {
+  /* margin-right: 14rem; */
 }
 </style>
