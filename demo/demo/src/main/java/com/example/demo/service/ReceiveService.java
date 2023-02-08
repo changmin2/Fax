99d41d4c62ce8d23b4149903852d2492634353b6
seulseulseulseul <laha0512@gmail.com>
2023-02-08 13:24:44 +0900

    메인화면 userId null 수정 및 받은팩스함 이름 나오게

--- demo/demo/src/main/java/com/example/demo/service/ReceiveService.java
+++ demo/demo/src/main/java/com/example/demo/service/ReceiveService.java
@@ -197,8 +197,8 @@ public class ReceiveService {
         return recieveRepository.findByFaxNo(FaxNo);
     }
 
-    public List<Recieve> findByFaxNo2(Map<String, String> map){
-        List<Recieve> result = new ArrayList<>();
+    public List<Map<String,Object>> findByFaxNo2(Map<String, String> map){
+        List<Map<String,Object>> result = new ArrayList<>();
         String FaxNo = map.get("RFax_No");
         String searchFrom = map.get("Date_Start");
         String searchTo = map.get("Date_End");
