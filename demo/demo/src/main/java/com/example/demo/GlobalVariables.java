f45eb790ee728c71d6f6a37f40466d21d6b828b9
LeeChangMin <77597971+changmin2@users.noreply.github.com>
2023-03-26 10:39:52 +0900

    Update GlobalVariables.java

--- demo/demo/src/main/java/com/example/demo/GlobalVariables.java
+++ demo/demo/src/main/java/com/example/demo/GlobalVariables.java
@@ -11,9 +11,9 @@ import java.util.Date;
 @Component
 @Getter
 public class GlobalVariables {
-    private String FaxId = "jungly5935";
-    private String FaxPw = "sjrnfl0814!";
-    private String filePath = "https://bnksys.s3.ap-northeast-2.amazonaws.com/";
+    private String FaxId = "*******";
+    private String FaxPw = "******";
+    private String filePath = "https://*****.s3.ap-northeast-2.amazonaws.com/";
     private String Service = "FAX";
 
     public String getNow(){
